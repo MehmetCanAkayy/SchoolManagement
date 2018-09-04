36eba5305992b73a771dc6a487b774401eef55e0
DESKTOP-860U835\MehmetCan <mehmetcan.akay4@gmail.com>
2018-09-04 18:16:22 +0300

    Teacher Register

--- app/src/main/java/com/user/TeacherRegister.java
+++ app/src/main/java/com/user/TeacherRegister.java
@@ -1,15 +1,83 @@
 package com.user;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.firebaseDemo.Artist;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jackandphantom.blurimage.BlurImage;
 import com.onur.easyspeakdemo.R;
 
+
+
 public class TeacherRegister extends AppCompatActivity {
+
+    private EditText editTextName;
+    private Spinner userGrade;
+    DatabaseReference databaseArtists;
+    private Button add;
+    private ImageView background;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_register);
+        setContentView(R.layout.activity_user_register);
+
+        editTextName= (EditText) findViewById(R.id.userName);
+        databaseArtists = FirebaseDatabase.getInstance().getReference("students");
+        background=findViewById(R.id.background);
+        add = findViewById(R.id.add);
+
+        userGrade = (Spinner) findViewById(R.id.user_grade);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.from, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        userGrade.setAdapter(adapter);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addArtist();
+            }
+        });
+        BlurImage.with(getApplicationContext()).load(R.drawable.backgorund).intensity(20).Async(true).into(background);
+
+
     }
+
+    private void addArtist(){
+        String name = editTextName.getText().toString().trim();
+        String grade = userGrade.getSelectedItem().toString();
+
+
+        if(!TextUtils.isEmpty(name)){
+            String id = databaseArtists.push().getKey();
+
+            //Create An Artist Object
+            Artist artist = new Artist(name,grade);
+            databaseArtists.child(id).setValue(artist);
+            Toast.makeText(this,"Succesfully Stored Data",Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(this,"You should enter a name",Toast.LENGTH_LONG).show();
+        }
+        finish();
+
+
+
+    }
+
+
+
+
 }
