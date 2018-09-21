22af6f2622d346b6123c3cc624722ee14d7c64f4
Onur <19.onurcelebi.95@gmail.com>
2018-09-21 17:28:32 +0300

    Kullanicinin survey secimi duzenlendi. Eger daha once secip onaylandiysa sadece onaylanan saatler gosterildi. Eger onaylanmadiysa saatleri guncelleme imkani icin eski panel gosterildi.

---
+++ app/src/main/java/com/StudentMenu/StudentSurveyApprovedActivity.java
@@ -0,0 +1,13 @@
+package com.StudentMenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentSurveyApprovedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_survey_approved);
+    }
+}
